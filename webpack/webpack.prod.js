4c3105d1eb0a752c3a1865f16d222f288badf9e8
jm <jm.dev.study@gmail.com>
2022-07-19 13:13:01 +0900

    🔧 production 모드의 devtool 변경 - 임시로 수정해뒀던 source-map을 다시 hidden-source-map 으로 변경
    Co-authored-by: mogooee <mogoo22@naver.com>

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: 'hidden-source-map',
   module: {
     rules: [
       {
