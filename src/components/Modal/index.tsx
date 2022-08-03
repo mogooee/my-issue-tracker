a6ae81e3948869fe4c9e6d2b54eec78f70248d51
jm <jm.dev.study@gmail.com>
2022-08-03 17:58:26 +0900

    ✨💄 독립적인 위치에서 생성되는 모달 컴포넌트 구현
    Co-authored-by: mogooee <mogoo22@naver.com>

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,20 @@
+import { atom } from 'recoil';
+import ModalPortal from '@/Portal';
+import { BackGround, ModalBlock } from '@/components/Modal/index.styled';
+
+export const ModalState = atom<boolean>({
+  key: 'ModalState',
+  default: false,
+});
+
+const Modal = ({ children }: any): JSX.Element => {
+  return (
+    <ModalPortal>
+      <BackGround>
+        <ModalBlock>{children}</ModalBlock>
+      </BackGround>
+    </ModalPortal>
+  );
+};
+
+export default Modal;
