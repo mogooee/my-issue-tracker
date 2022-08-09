07a548ddd207b1e2d8ca5434b24779f8f50c03b7
연 <mogoo22@naver.com>
2022-08-09 14:23:25 +0900

    🩹 api 응답에 따른 변수명 수정 - profileImageUrl->profileImage - loginId -> id

--- src/components/Atoms/UserImage/UserImage.stories.tsx
+++ src/components/Atoms/UserImage/UserImage.stories.tsx
@@ -10,13 +10,14 @@ const Template: ComponentStory<typeof UserImage> = (args) => <UserImage {...args
 
 export const Medium = Template.bind({});
 Medium.args = {
-  loginId: '도톨',
-  profileImageUrl: 'https://avatars.githubusercontent.com/u/92701121?v=4',
+  id: 0,
+  profileImage: 'https://avatars.githubusercontent.com/u/92701121?v=4',
   imgSize: 'MEDIUM',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  loginId: '도비',
-  profileImageUrl: 'https://avatars.githubusercontent.com/u/85747667?s=96&v=4',
+  id: 1,
+  profileImage: 'https://avatars.githubusercontent.com/u/85747667?s=96&v=4',
+  imgSize: 'SMALL',
 };
--- src/components/Atoms/UserImage/index.tsx
+++ src/components/Atoms/UserImage/index.tsx
@@ -2,15 +2,15 @@ import * as S from '@/components/Atoms/UserImage/index.styles';
 
 export interface UserImageTypes {
   id: number;
-  profileImageUrl: string;
+  profileImage: string;
   imgSize?: 'MEDIUM' | 'SMALL';
 }
 
 const UserImage = ({ imgSize = 'SMALL', ...props }: UserImageTypes) => {
-  const { profileImageUrl, id } = props;
+  const { profileImage, id } = props;
   const imgAlt = `${id}의 프로필 사진`;
 
-  return <S.Img src={profileImageUrl} alt={imgAlt} imgSize={imgSize} />;
+  return <S.Img src={profileImage} alt={imgAlt} imgSize={imgSize} />;
 };
 
 export default UserImage;
