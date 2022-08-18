1b75d5f828e586eef6e463335e1072d6d2385a63
jm <jm.dev.study@gmail.com>
2022-08-18 16:23:17 +0900

    ✨ 일반 로그인 실패시 해당하는 메세지를 보여주는 기능 추가
    Co-authored-by: mogooee <mogoo22@naver.com>

--- src/components/Molecules/CommonLoginForm/index.tsx
+++ src/components/Molecules/CommonLoginForm/index.tsx
@@ -16,6 +16,17 @@ const Form = styled.div`
   }
 `;
 
+const FailMessage = styled.span`
+  color: ${({ theme }) => theme.COLORS.ERROR.RED};
+  ${({ theme }) => theme.FONTSTYLES.TEXT_XSMALL};
+  display: block;
+  text-align: center;
+  padding-bottom: 24px;
+`;
+
+const initLoginForm = { id: '', password: '' };
+const [idMaxLength, passwordMaxLength] = [10, 10];
+
 const LoginForm = (): JSX.Element => {
   const {
     isActive: isIdActive,
@@ -32,18 +43,19 @@ const LoginForm = (): JSX.Element => {
     onBlurInput: onBlurInputPassword,
   } = useInput();
 
-  const [idMaxLength, passwordMaxLength] = [10, 10];
-
   const navigate = useNavigate();
   const { onSuccessLogin } = useLogin();
-
-  const initLoginForm = { id: '', password: '' };
   const [loginForm, setLoginForm] = useState(initLoginForm);
+  const [isError, setIsError] = useState<boolean>(false);
 
   const login = async () => {
-    const { memberResponse } = (await generalLogin(loginForm)) as OAuthResponse;
-    onSuccessLogin(memberResponse);
-    navigate('/issues');
+    try {
+      const { memberResponse } = (await generalLogin(loginForm)) as OAuthResponse;
+      onSuccessLogin(memberResponse);
+      navigate('/issues');
+    } catch (error) {
+      setIsError(true);
+    }
   };
 
   return (
@@ -76,6 +88,12 @@ const LoginForm = (): JSX.Element => {
         inputMaxLength={passwordMaxLength}
         inputPlaceholder="비밀번호"
       />
+      {isError && (
+        <FailMessage>
+          아이디 또는 비밀번호를 잘못 입력했습니다.
+          <br /> 입력하신 내용을 다시 확인해주세요.
+        </FailMessage>
+      )}
       <Button buttonStyle="STANDARD" label="아이디로 로그인" size="LARGE" handleOnClick={login} />
     </Form>
   );
