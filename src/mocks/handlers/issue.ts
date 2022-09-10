a0775ded0e0b12ddbe0c8f9702a726e9030c6f63
연 <mogoo22@naver.com>
2022-09-10 15:07:08 +0900

    🤡 이슈 단건 조회 및 이슈 제목 수정 API 모킹 - import 한 이슈 테이블은 수정이 불가하므로 issueTable으로 let 선언하여 사용한다.

--- src/mocks/handlers/issue.ts
+++ src/mocks/handlers/issue.ts
@@ -7,14 +7,30 @@ const message = {
   message: '',
 };
 
+let issueTable = issues;
+
 export const issueHandlers = [
   // 이슈 전체 조회
-  rest.get('api/issues', (req, res, ctx) => res(ctx.status(200), ctx.json(issues))),
+  rest.get('api/issues', (req, res, ctx) => res(ctx.status(200), ctx.json(issueTable))),
 
   // 이슈 단건 조회
-  rest.get('api/issues/:id', (req, res, ctx) => {
+  rest.get('api/issues/:issueId', (req, res, ctx) => {
+    const { issueId } = req.params;
+    const { openIssues, closedIssues } = issueTable;
+    const contents = [...openIssues.content, ...closedIssues.content];
+    const issue = contents.find((e) => e.id === Number(issueId));
+
+    if (!issue) {
+      message.message = '해당하는 이슈 데이터가 없습니다.';
+      return res(ctx.status(400), ctx.json(message));
+    }
+    return res(ctx.status(200), ctx.json(issue));
+  }),
+
+  // 이슈 제목 수정
+  rest.patch('api/issues/:id/title', async (req, res, ctx) => {
     const { id } = req.params;
-    const { openIssues, closedIssues } = issues;
+    const { openIssues, closedIssues } = issueTable;
     const contents = [...openIssues.content, ...closedIssues.content];
     const issue = contents.find((e) => e.id === Number(id));
 
@@ -22,7 +38,23 @@ export const issueHandlers = [
       message.message = '해당하는 이슈 데이터가 없습니다.';
       return res(ctx.status(400), ctx.json(message));
     }
-    return res(ctx.status(200), ctx.json(issue));
+
+    const { title: newTitle } = await req.json();
+
+    const newIssue = { ...issue, title: newTitle };
+
+    let replacedContent = issue.closed ? issues.closedIssues.content : issues.openIssues.content;
+    replacedContent = replacedContent.map((content) => {
+      if (content.id === Number(id)) return newIssue;
+      return content;
+    });
+
+    if (issue.closed) issueTable = { ...issueTable, closedIssues: { ...closedIssues, content: replacedContent } };
+    else issueTable = { ...issueTable, openIssues: { ...openIssues, content: replacedContent } };
+
+    return res(ctx.status(200), ctx.json(newIssue));
+  }),
+
   }),
 
   // 이모티콘 전체 조회
