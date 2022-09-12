c72389a6e94506ed20b4d23aef6f579815587922
연 <mogoo22@naver.com>
2022-09-12 18:48:08 +0900

    🤡 이슈 코멘트 리액션 추가, 삭제 목API 구현

--- src/mocks/handlers/issue.ts
+++ src/mocks/handlers/issue.ts
@@ -2,6 +2,7 @@
 import { rest } from 'msw';
 import { issues } from '@/mocks/tables/issue';
 import { REACTIONS } from '@/components/Molecules/Dropdown/Panel/Reaction/mock';
+import { CommentsTypes, ContentTypes, ReactionResponseTypes } from '@/api/issue/types';
 import { userTable } from '@/mocks/handlers/auth';
 
 const message = {
@@ -31,19 +32,36 @@ const updateIssueTable = (newIssue: ContentTypes) => {
   else issueTable = { ...issueTable, openIssues: { ...issueTable.openIssues, content: replacedIssuesContent } };
 };
 
-const addIdCount = (type: 'comments') => {
+const addIdCount = (type: 'comments' | 'reactions') => {
   const contents = [...issueTable.openIssues.content, ...issueTable.closedIssues.content];
 
   let commentsCount = contents.reduce((acc, cur) => acc + cur.comments.length, 0);
+
+  let reactionsCount: number = contents.reduce(
+    (accContentReactions, { comments }) =>
+      accContentReactions +
+      comments.reduce(
+        (accCommentReactions, { issueCommentReactionsResponse }) =>
+          accCommentReactions + issueCommentReactionsResponse.length,
+        0,
+      ),
+    0,
+  );
 
   return () => {
     if (type === 'comments') {
       commentsCount += 1;
       return commentsCount;
     }
+
+    reactionsCount += 1;
+    return reactionsCount;
+  };
 };
 
 const addCommentsId = addIdCount('comments');
+const addReactionsId = addIdCount('reactions');
+
 export const issueHandlers = [
   // 이슈 전체 조회
   rest.get('api/issues', (req, res, ctx) => res(ctx.status(200), ctx.json(issueTable))),
@@ -190,4 +208,71 @@ export const issueHandlers = [
 
   // 이모티콘 전체 조회
   rest.get('api/issues/comments/reactions/emojis', (req, res, ctx) => res(ctx.status(200), ctx.json(REACTIONS))),
+
+  // 이슈 코멘트 리액션 추가
+  rest.post('api/issues/:issueId/comments/:commentId/reactions/:emojiName', (req, res, ctx) => {
+    const { issueId, commentId, emojiName } = req.params;
+    const memberId = Number(req.url.searchParams.get('memberId'));
+    const issue = findIssue(Number(issueId));
+
+    if (!issue) {
+      message.message = '해당하는 이슈 데이터가 없습니다.';
+      return res(ctx.status(400), ctx.json(message));
+    }
+
+    const newReactionId = addReactionsId();
+    const { unicode } = REACTIONS.find((e) => emojiName === e.name)!;
+    const newReactor = { id: memberId, nickname: userTable.find(({ id }) => id === memberId)?.nickname! };
+
+    const newReaction: ReactionResponseTypes = {
+      id: newReactionId,
+      emoji: unicode,
+      issueCommentReactorResponse: newReactor,
+    };
+
+    const newComments: CommentsTypes[] = issue.comments.map((comment) => {
+      if (comment.id === Number(commentId)) {
+        return {
+          ...comment,
+          issueCommentReactionsResponse: [...comment.issueCommentReactionsResponse, { ...newReaction }],
+        };
+      }
+      return comment;
+    });
+
+    const newIssue: ContentTypes = { ...issue, comments: newComments };
+    updateIssueTable(newIssue);
+
+    return res(ctx.status(200), ctx.json(newIssue));
+  }),
+
+  // 이슈 코멘트 리액션 삭제
+  rest.delete('api/issues/:issueId/comments/:commentId/reactions/:reactionId', (req, res, ctx) => {
+    const { issueId, commentId, reactionId } = req.params;
+    const issue = findIssue(Number(issueId));
+
+    if (!issue) {
+      message.message = '해당하는 이슈 데이터가 없습니다.';
+      return res(ctx.status(400), ctx.json(message));
+    }
+
+    const newComments: CommentsTypes[] = issue.comments.map((comment) => {
+      if (comment.id === Number(commentId)) {
+        const newReactions: ReactionResponseTypes[] = comment.issueCommentReactionsResponse.filter(
+          ({ id }) => id !== Number(reactionId),
+        );
+
+        return {
+          ...comment,
+          issueCommentReactionsResponse: newReactions,
+        };
+      }
+      return comment;
+    });
+
+    const newIssue: ContentTypes = { ...issue, comments: newComments };
+    updateIssueTable(newIssue);
+
+    return res(ctx.status(200), ctx.json(newIssue));
+  }),
 ];
