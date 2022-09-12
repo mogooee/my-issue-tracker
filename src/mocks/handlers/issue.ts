2a69a308f9f1d3cfb2c619f49b0cf658e1e5be99
연 <mogoo22@naver.com>
2022-09-12 16:33:09 +0900

    ♻️ issues 목API의 중복되는 findIssue, updateIssueTable 함수 리팩토링

--- src/mocks/handlers/issue.ts
+++ src/mocks/handlers/issue.ts
@@ -9,6 +9,26 @@ const message = {
 
 let issueTable = issues;
 
+const findIssue = (issueId: number) => {
+  const { openIssues, closedIssues } = issueTable;
+  const contents = [...openIssues.content, ...closedIssues.content];
+
+  const issue = contents.find((e) => e.id === Number(issueId));
+  return issue;
+};
+
+const updateIssueTable = (newIssue: ContentTypes) => {
+  let replacedIssuesContent = newIssue.closed ? issueTable.closedIssues.content : issueTable.openIssues.content;
+
+  replacedIssuesContent = replacedIssuesContent.map((content) => {
+    if (content.id === newIssue.id) return newIssue;
+    return content;
+  });
+
+  if (newIssue.closed)
+    issueTable = { ...issueTable, closedIssues: { ...issueTable.closedIssues, content: replacedIssuesContent } };
+  else issueTable = { ...issueTable, openIssues: { ...issueTable.openIssues, content: replacedIssuesContent } };
+};
 export const issueHandlers = [
   // 이슈 전체 조회
   rest.get('api/issues', (req, res, ctx) => res(ctx.status(200), ctx.json(issueTable))),
@@ -16,9 +36,8 @@ export const issueHandlers = [
   // 이슈 단건 조회
   rest.get('api/issues/:issueId', (req, res, ctx) => {
     const { issueId } = req.params;
-    const { openIssues, closedIssues } = issueTable;
-    const contents = [...openIssues.content, ...closedIssues.content];
-    const issue = contents.find((e) => e.id === Number(issueId));
+
+    const issue = findIssue(Number(issueId));
 
     if (!issue) {
       message.message = '해당하는 이슈 데이터가 없습니다.';
@@ -28,28 +47,23 @@ export const issueHandlers = [
   }),
 
   // 이슈 제목 수정
-  rest.patch('api/issues/:id/title', async (req, res, ctx) => {
-    const { id } = req.params;
-    const { openIssues, closedIssues } = issueTable;
-    const contents = [...openIssues.content, ...closedIssues.content];
-    const issue = contents.find((e) => e.id === Number(id));
+  rest.patch('api/issues/:issueId/title', async (req, res, ctx) => {
+    const { issueId } = req.params;
+    const issue = findIssue(Number(issueId));
 
     if (!issue) {
       message.message = '해당하는 이슈 데이터가 없습니다.';
       return res(ctx.status(400), ctx.json(message));
     }
 
-    const { title: newTitle } = await req.json();
-    const newIssue = { ...issue, title: newTitle };
+    const { title } = await req.json();
 
-    let replacedContent = issue.closed ? issues.closedIssues.content : issues.openIssues.content;
-    replacedContent = replacedContent.map((content) => {
-      if (content.id === Number(id)) return newIssue;
-      return content;
-    });
+    if (!title) {
+      message.message = '이슈 제목이 유효하지 않습니다.';
+    }
 
-    if (issue.closed) issueTable = { ...issueTable, closedIssues: { ...closedIssues, content: replacedContent } };
-    else issueTable = { ...issueTable, openIssues: { ...openIssues, content: replacedContent } };
+    const newIssue = { ...issue, title };
+    updateIssueTable(newIssue);
 
     return res(ctx.status(200), ctx.json(newIssue));
   }),
