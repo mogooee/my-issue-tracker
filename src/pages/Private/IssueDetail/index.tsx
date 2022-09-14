352a6dc3ea30c2d51de9763670cf2dcc56dfcd5b
연 <mogoo22@naver.com>
2022-09-14 16:57:59 +0900

    🩹 머지과정에서 수정된 textArea prop을 이슈 상세페이지의NewComment에 적용함

--- src/pages/Private/IssueDetail/index.tsx
+++ src/pages/Private/IssueDetail/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { useParams } from 'react-router-dom';
 import { LoginUserInfoState } from '@/stores/loginUserInfo';
@@ -16,6 +16,7 @@ import SideBar from '@/components/Molecules/SideBar';
 import IssueHeader from '@/components/Organisms/IssueHeader';
 import Modal, { ModalState } from '@/components/Modal';
 import DeleteCheck from '@/components/Modal/DeleteCheck';
+import { DEFAULT_TEXTAREA_MAX_LENGTH } from '@/components/Molecules/TextAreaEditer/constants';
 
 const IssueDetail = (): JSX.Element => {
   const { issueId } = useParams();
@@ -44,6 +45,16 @@ const IssueDetail = (): JSX.Element => {
   const handleDeleteCommentButton = () => {
     deleteIssueComment({ issueId: id, commentId: selectCommentId, memberId });
     setIsDeleteModalOpen(false);
+  };
+
+  const handleOnChangeNewComment = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = event.target;
+    if (!value) return setTextAreaValue('');
+    if (Number(value) >= DEFAULT_TEXTAREA_MAX_LENGTH) {
+      // eslint-disable-next-line no-param-reassign
+      event.target.value = value.slice(0, DEFAULT_TEXTAREA_MAX_LENGTH);
+    }
+    return setTextAreaValue(value);
   };
 
   return (
@@ -76,122 +87,11 @@ const IssueDetail = (): JSX.Element => {
           })}
           <S.NewComment>
             <UserImage {...userInfo} imgSize="MEDIUM" />
-            <TextArea textAreaValue={textAreaValue} setAreaValue={setTextAreaValue} />
+            <TextArea textAreaValue={textAreaValue} handleOnChange={handleOnChangeNewComment} />
           </S.NewComment>
           <Button {...BUTTON_PROPS.ADD} disabled={isTypingNewComment} handleOnClick={handleAddCommentButton} />
         </S.IssueComments>
         <S.Aside>
-          <SideBar
-            content={{
-              assignee: [],
-              label: [],
-              milestone: [],
-            }}
-            sideBarList={[
-              {
-                dropdownList: [
-                  {
-                    email: 'dobby@gmail.com',
-                    id: 0,
-                    nickname: '도비',
-                    profileImage: 'https://avatars.githubusercontent.com/u/85747667?v=4',
-                  },
-                  {
-                    email: 'dotori@gmail.com',
-                    id: 1,
-                    nickname: '도토리',
-                    profileImage: 'https://avatars.githubusercontent.com/u/92701121?v=4',
-                  },
-                  {
-                    email: 'whoo@gmail.com',
-                    id: 2,
-                    nickname: '후',
-                    profileImage: 'https://avatars.githubusercontent.com/u/68011320?v=4',
-                  },
-                  {
-                    email: 'ader@gmail.com',
-                    id: 3,
-                    nickname: '아더',
-                    profileImage: 'https://avatars.githubusercontent.com/u/29879110?v=4',
-                  },
-                  {
-                    email: 'beck@gmail.com',
-                    id: 4,
-                    nickname: '벡',
-                    profileImage: 'https://avatars.githubusercontent.com/u/65931336?v=4',
-                  },
-                ],
-                dropdownListTitle: '담당자 필터',
-                dropdownTitle: '담당자',
-                dropdownType: 'checkbox',
-                id: 'assignee',
-              },
-              {
-                dropdownList: [
-                  {
-                    backgroundColorCode: '#007AFF',
-                    description: '',
-                    id: 0,
-                    textColor: 'BLACK',
-                    title: 'feature',
-                  },
-                  {
-                    backgroundColorCode: '#FFD1CF',
-                    description: '',
-                    id: 1,
-                    textColor: 'BLACK',
-                    title: 'Fix',
-                  },
-                  {
-                    backgroundColorCode: '#34C759',
-                    description: '',
-                    id: 2,
-                    textColor: 'BLACK',
-                    title: 'refactor',
-                  },
-                ],
-                dropdownListTitle: '레이블 필터',
-                dropdownTitle: '레이블',
-                dropdownType: 'checkbox',
-                id: 'label',
-              },
-              {
-                dropdownList: [
-                  {
-                    closed: false,
-                    closedIssueCount: 7,
-                    description: null,
-                    dueDate: null,
-                    id: 0,
-                    openIssueCount: 3,
-                    title: '마일스톤 1',
-                  },
-                  {
-                    closed: true,
-                    closedIssueCount: 13,
-                    description: '닫힌 마일스톤에 대한 설명',
-                    dueDate: null,
-                    id: 1,
-                    openIssueCount: 16,
-                    title: '마일스톤 2',
-                  },
-                  {
-                    closed: false,
-                    closedIssueCount: 5,
-                    description: '열린 마일스톤에 대한 설명',
-                    dueDate: '2022-08-28',
-                    id: 2,
-                    openIssueCount: 5,
-                    title: '마일스톤 3',
-                  },
-                ],
-                dropdownListTitle: '마일스톤 필터',
-                dropdownTitle: '마일스톤',
-                dropdownType: 'radio',
-                id: 'milestone',
-              },
-            ]}
-          />
           {isIssueAuthor && (
             <Button
               buttonStyle="NO_BORDER"
