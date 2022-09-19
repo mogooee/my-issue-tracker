1efd102111a2fa39ee0df6c2d9a278944e125a30
연 <mogoo22@naver.com>
2022-09-19 18:47:33 +0900

    ✨ filterState가 변경됨에 따라 navigate훅을 이용하여 필터 쿼리문을 적용한 url로 이동한다.

--- src/stores/filter.ts
+++ src/stores/filter.ts
@@ -1,4 +1,5 @@
 import { atom, selector } from 'recoil';
+import { LoginUserInfoState } from '@/stores/loginUserInfo';
 
 export type IssueStateType = 'open' | 'closed' | 'all';
 export type IssueAboutMyselfType = 'me';
@@ -35,12 +36,51 @@ export const PageState = atom<number>({
   default: 0,
 });
 
+const encodeSpace = (string: string): string => {
+  const spaceReg = /\s/g;
+
+  if (string.match(spaceReg)) {
+    return `${string.replaceAll(' ', '+')}`;
+  }
+
+  return string;
+};
+
+const encodeQueryString = (value: string): string => encodeURIComponent(encodeSpace(value));
+
 export const FilterStatsState = selector({
   key: 'FilterStatsState',
   get: ({ get }) => {
     const filterState = get(FilterState);
+    const userInfo = get(LoginUserInfoState);
+    const page = get(PageState);
+
     const isFiltering = JSON.stringify(filterState) !== JSON.stringify(initFilterState);
 
-    return { isFiltering };
+    const filterQueryString = Object.keys(filterState)
+      .reduce((acc, key) => {
+        const value = filterState[key];
+
+        if (Array.isArray(value)) {
+            return (
+              acc +
+              value.reduce((accState: string, curValue: string) => {
+                const newQuery = key === 'label' ? `${key}:"${curValue}"` : `${curValue}:"${''}"`;
+                return `${accState} ${encodeQueryString(newQuery)}`;
+              }, '')
+            );
+        }
+        if (value) {
+          if (key === 'is' && value === 'all') return acc;
+            const filterValue = value === '@me' ? userInfo.nickname : value;
+            const newQuery = `${key}:"${filterValue}"`;
+            return `${acc} ${encodeQueryString(newQuery)}`;
+          }
+        return acc;
+      }, '')
+      .trim();
+    const quries = filterQueryString.replaceAll(' ', '+');
+
+    return { isFiltering, page, quries };
   },
 });
