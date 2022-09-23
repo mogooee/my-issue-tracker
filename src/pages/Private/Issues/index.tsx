eab5474dcf0330749123ca29b78a442e1a9da6c8
연 <mogoo22@naver.com>
2022-09-23 14:18:23 +0900

    ♻️ FilterState를 추가, 제거하는 로직을 useFilter 훅으로 분리함

--- src/pages/Private/Issues/index.tsx
+++ src/pages/Private/Issues/index.tsx
@@ -1,12 +1,10 @@
 import { useEffect } from 'react';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import useFetchIssue from '@/api/issue/useFetchIssue';
 
 import * as S from '@/pages/Private/Issues/index.styled';
-import { COLORS } from '@/styles/theme';
 
-import Icon from '@/components/Atoms/Icon';
 import Button from '@/components/Atoms/Button';
 import FilterBar from '@/components/Molecules/FilterBar';
 import NavLink from '@/components/Molecules/NavLink';
@@ -20,26 +18,30 @@ import {
   MILESTONE_DROPDOWN_ARGS,
 } from '@/components/Molecules/Dropdown/mock';
 import { NEW_ISSUE_BUTTON_INFO } from '@/components/Atoms/Button/options';
-import { FilterState, FilterStatsState, PageState } from '@/stores/filter';
+import { FilterState, FilterStatsState, initFilterState, PageState } from '@/stores/filter';
 import useFetchLabel from '@/api/label/useFetchLabel';
 import useFetchMilestone from '@/api/milestone/useFetchMilestone';
 import useFetchSideBarData from '@/api/useFetchSideBarData';
+import useFilter, { parsingFilterReg } from '@/hooks/useFilter';
+import { labelMilestone } from '@/components/Molecules/NavLink/options';
 
 const Issues = () => {
   const naviagate = useNavigate();
   const [searchParams] = useSearchParams();
-  const quriesParams = searchParams.get('q') || '';
   const pageParams = Number(searchParams.get('page')) || 0;
-
-  const [filterState, setFilterState] = useRecoilState(FilterState);
-  const { page, quries } = useRecoilValue(FilterStatsState);
+  const queriesParams = searchParams.get('q');
+  const filterState = useRecoilValue(FilterState);
+  const { page, queries } = useRecoilValue(FilterStatsState);
   const setPageState = useSetRecoilState(PageState);
 
   const { labelData } = useFetchLabel();
   const { milestoneData } = useFetchMilestone();
   const { memberData } = useFetchSideBarData();
   const { useIssuesData } = useFetchIssue();
-  const { data: issues } = useIssuesData(pageParams, quriesParams);
+
+  const { data: issues } = useIssuesData(pageParams, queriesParams);
+
+  const { setIssueState, setParsingFilterState } = useFilter();
 
   const filterTabs = [
     ASSIGNEE_DROPDOWN_ARGS(memberData!),
@@ -48,9 +50,16 @@ const Issues = () => {
     AUTHOR_DROPDOWN_ARGS(memberData!),
   ];
 
-  useEffect(() => {
-    setCheckState((checkState) => ({ ...checkState, issueState: filterState.is }));
-  }, [filterState.state]);
+  const setURLQueriesToFilterState = () => {
+    if (!document.location.search) return;
+
+    setIssueState(queriesParams);
+
+    const queriesArr = queriesParams?.match(parsingFilterReg);
+    queriesArr?.forEach((query) => {
+      setParsingFilterState(query);
+    });
+  };
 
   useEffect(() => {
 
@@ -59,19 +68,7 @@ const Issues = () => {
 
   useEffect(() => {
     setPageState(pageParams);
-    if (quriesParams) {
-      const queriesArr = quriesParams.split(' ');
-      queriesArr.forEach((qurey) => {
-        const [key, value] = qurey.split(':');
-        const filterValue = value.replace(/(^"|"$)/g, '');
-        setFilterState((prev) => {
-          if (!filterValue) return { ...prev, no: [...prev.no, key as 'label' | 'milestone' | 'assignee'] };
-
-          const newValue = key === 'label' ? [...prev[key], filterValue] : filterValue;
-          return { ...prev, [key]: newValue };
-        });
-      });
-    }
+    setURLQueriesToFilterState();
   }, []);
 
   return (
