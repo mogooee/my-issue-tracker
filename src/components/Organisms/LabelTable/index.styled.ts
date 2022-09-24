b83add2dc9a1a138d460c1d50a9aa5047236e894
연 <mogoo22@naver.com>
2022-09-24 13:18:16 +0900

    🩹 LabelEditForm의 취소버튼 클릭 핸들러 연결 및 벗어나는 레이아웃 수정

--- src/components/Organisms/LabelTable/index.styled.ts
+++ src/components/Organisms/LabelTable/index.styled.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 import { LabelEditForm } from '@/components/Molecules/LabelEditForm/index.styled';
+import { Table } from '@/components/Molecules/Table/index.styled';
 
 export const LabelTable = styled.div`
   ${LabelEditForm} {
     border: none;
+    padding: 0px 64px 0px 0px;
+  }
+
+  ${Table} {
+    & > div {
+      overflow: hidden;
+    }
   }
 `;
