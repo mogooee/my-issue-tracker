cf5c3523302ce9321da136a0b3a7c70ad4d07a5a
연 <mogoo22@naver.com>
2022-10-03 23:26:15 +0900

    🔥 interceptor 로직 제거
    Co-authored-by: 도비 <JiminKim-dev@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import axios, { AxiosError } from 'axios';
 import App from './App';
 import { worker } from './mocks/worker';
-import { silentRefresh } from './api/sign';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start({ onUnhandledRequest: 'bypass' });
 }
-
-interface ErrorMessage {
-  message: string;
-}
-
-export const unAuthorizedErrorMsg = {
-  refreshToken: '유효하지 않은 refresh_token입니다.',
-  accessToken: '요청에 Authorization 헤더가 존재하지 않습니다.',
-};
-
-axios.defaults.withCredentials = true;
-
-axios.interceptors.response.use(
-  (response) => response,
-  (error: AxiosError<ErrorMessage>) => {
-    const status = error.response?.status;
-    const { message } = error.response!.data;
-    if (status === 401 && message === unAuthorizedErrorMsg.accessToken) {
-      silentRefresh();
-    }
-    if (status === 401 && message === unAuthorizedErrorMsg.refreshToken) {
-      // 메인으로 이동
-    }
-    return Promise.reject(error);
-  },
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
