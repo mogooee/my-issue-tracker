0f28168cb97b466db477226d59a083783f44c1cd
연 <mogoo22@naver.com>
2022-10-07 16:57:45 +0900

    🚚 initLabelState를 store에서 LabelEditForm 디렉토리의 constant 파일로 이동

--- src/components/Molecules/LabelEditForm/constants.tsx
+++ src/components/Molecules/LabelEditForm/constants.tsx
@@ -1,10 +1,11 @@
 import { LabelTypes } from '@/api/issue/types';
 import { COLORS } from '@/styles/theme';
 
-const initLabelState: LabelTypes = {
+export const initLabelState: LabelTypes = {
   id: 0,
   title: '',
   backgroundColorCode: COLORS.INPUT_BACKGROUND,
   description: '',
   textColor: 'BLACK',
 };
+
