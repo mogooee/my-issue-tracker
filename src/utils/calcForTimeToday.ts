022ac92a81ca74d450395fc74308bfcf3a7c5a77
jm <jm.dev.study@gmail.com>
2022-12-03 03:53:01 +0900

    ♻️ 특정 시간이 지난 후 날짜 표기법 변경 - 이번 달 날짜가 아닌 경우 부터 월일로 표기된다. - 년도가 변경되면 년월일로 표기된다.

--- src/utils/calcForTimeToday.ts
+++ src/utils/calcForTimeToday.ts
@@ -1,24 +1,28 @@
+const dateConverter = (today: Date, inputDate: Date) => {
+  const year = inputDate.getFullYear();
+  const month = inputDate.getMonth() + 1;
+  const date = inputDate.getDate();
+
+  return today.getFullYear() === year ? `${month}월 ${date}일` : `${year}년 ${month}월 ${date}일`;
+};
+
 const calcTimeForToday = (timeStampValue: string) => {
   const today = new Date();
   const timeStamp = new Date(timeStampValue);
   const timeDifference = today.getTime() - timeStamp.getTime();
-  const [milliSecond, second, minute, hour, day, month, year] = [1000, 1, 60, 60, 24, 30, 12];
+  const [milliSecond, second, minute, hour, day, month] = [1000, 1, 60, 60, 24, 30];
 
   const minuteDifference = Math.floor(timeDifference / milliSecond / minute);
-  if (minuteDifference < second) return '방금전';
-  if (minuteDifference < hour) return `${minuteDifference}분전`;
+  if (minuteDifference < second) return '방금 전';
+  if (minuteDifference < hour) return `${minuteDifference}분 전`;
 
   const hourDifference = Math.floor(minuteDifference / hour);
-  if (hourDifference < day) return `${hourDifference}시간전`;
+  if (hourDifference < day) return `${hourDifference}시간 전`;
 
   const dayDifference = Math.floor(hourDifference / day);
-  if (dayDifference < month) return `${dayDifference}일전`;
+  if (dayDifference < month && today.getMonth() === timeStamp.getMonth()) return `${dayDifference}일 전`;
 
-  const monthDifference = Math.floor(dayDifference / month);
-  if (monthDifference < year) return `${monthDifference}개월전`;
-
-  const yearDifference = Math.floor(monthDifference / year);
-  return `${yearDifference}년전`;
+  return dateConverter(today, timeStamp);
 };
 
 export default calcTimeForToday;
