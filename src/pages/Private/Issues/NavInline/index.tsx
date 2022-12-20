c526721b3ee6a356d1f91f1cc2b9f04008bb4f57
도비 <jm.dev.study@gmail.com>
2022-12-20 15:42:49 +0900

    로그인 후 UI 흐름 수정 및 이슈테이블 스켈레톤 UI 적용 (#52)
    * ♻️ Issue 컴포넌트에서 이슈 테이블을 제외한 컴포넌트 분리
    - 레이블, 마일스톤 카운트 삭제
    - 레이블, 마일스톤 데이터를 TableInfoTabs 컴포넌트에서 refetch 하도록 수정
    
    * ✨ 이슈테이블의 스켈레톤 UI 구현 및 적용
    - 스켈레톤 이슈테이블 스토리 적용
    
    * 🐛 PrivateRouter의 /redirect-auth 경로에 대한 Route 추가
    - /redirect-auth 경로에 Navigate 컴포넌트를 넣어서 해당 경로 접근시 /issues 경로로 이동하도록 수정
    
    * 🐛 PublicRouter에서 허용하지 않은 경로 접근시 루트 경로로 이동하도록 수정
    - Navigate 컴포넌트로 루트 경로로 이동하게 함
    
    * 💄 최상위 suspense의 fallback UI 추가
    - 로딩스피너와 함께 "서비스를 로딩중입니다." 라는 문구를 보여준다.

---
+++ src/pages/Private/Issues/NavInline/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import * as S from '@/pages/Private/Issues/NavInline/index.styled';
+
+import FilterBar from '@/components/Molecules/FilterBar';
+import { FILTERBAR_INFO } from '@/components/Molecules/FilterBar/mocks';
+
+import NavLink from '@/components/Molecules/NavLink';
+import { labelMilestone } from '@/components/Molecules/NavLink/options';
+
+import Button from '@/components/Atoms/Button';
+import { NEW_ISSUE_BUTTON_INFO } from '@/components/Atoms/Button/options';
+
+const IssuesNavInline = () => (
+  <S.NavInline>
+    <FilterBar {...FILTERBAR_INFO} />
+    <S.SubNav>
+      <NavLink navData={labelMilestone(0, 0)} navLinkStyle="LINE" />
+      <Link to="/issues/new">
+        <Button {...NEW_ISSUE_BUTTON_INFO.WRITE} />
+      </Link>
+    </S.SubNav>
+  </S.NavInline>
+);
+
+export default IssuesNavInline;
