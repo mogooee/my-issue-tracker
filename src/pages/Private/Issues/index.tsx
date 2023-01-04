1aa80f12bbedb54eb3efccdb8449de732a855f47
mogooee <mogoo22@naver.com>
2023-01-04 15:28:25 +0900

    ♻️ 검색 순으로 필터바에 렌더링 및 마일스톤 페이지 필터 기능 구현 (#55)
    * ✨ 마일스톤 페이지에서 마일스톤 제목을 클릭하여 이슈를 필터조회할 수 있다.
    
    * ♻️ OPEN_QUERY, CLOSED_QUERY 상수 분리
    
    * ♻️ 필터상태를 객체에서 string 타입으로 변경
    
    * ♻️ 필터탭을 클릭하거나 검색어를 입력하면 훅에서 필터 정렬 후 navigate로 쿼리를 담아 이동한다.
    
    * ♻️ 레이블 및 마일스톤을 클릭하면 제목이 영문자가 아닌 경우 따옴표("")를 붙여 필터 검색한다.
    - 이슈페이지의 이슈 테이블에서 레이블 아이콘 클릭
    - 레이블페이지에서 레이블 아이콘 클릭
    - 마일스톤페이지에서 마일스토 제목 클릭
    
    * ♻️ 필터 쿼리를 URL로 인코딩하고 API 요청 조건에 맞게 변환하는 로직을 훅으로 분리
    
    * ♻️ 이슈테이블의 이슈 상태 변경탭 클릭시 이동할 url의 상태값을 변경한다.
    
    * 🐛 디코딩한 쿼리문으로 이슈테이블 상태탭 활성화를 확인한다.
    - API 요청 쿼리문이 두번 인코딩한 값이므로 한 번 디코딩하여 비교한다.
    
    * ♻️ url이 변경됨에 따라 새로운 페이지, 쿼리 상태값을 저장하고 이슈 목록을 렌더링한다.
    
    * ♻️ 드롭다운 탭 리스트의 글자외 공백을 클릭해도 기능이 작동하도록 padding 수정

--- src/pages/Private/Issues/index.tsx
+++ src/pages/Private/Issues/index.tsx
@@ -1,72 +1,42 @@
 import { Suspense, useEffect } from 'react';
-import { useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 import useFetchIssue from '@/api/issue/useFetchIssue';
+import { OPEN_QUERY } from '@/hooks/useFilter';
+import { FilterState, PageState } from '@/stores/filter';
+
 import IssuesNavInline from '@/pages/Private/Issues/NavInline';
 import IssueTable from '@/components/Organisms/IssueTable';
-
-import { FilterState, FilterStatsState, initFilterState, PageState } from '@/stores/filter';
-
-import useFilter, { parsingFilterReg } from '@/hooks/useFilter';
 import Paginiation from '@/components/Organisms/Pagination';
 import SkeletonIssueTable from '@/components/Skeleton/IssueTable';
 
 const Issues = () => {
-  const navigate = useNavigate();
-
   const [searchParams] = useSearchParams();
-  const pageParams = Number(searchParams.get('page')) || 0;
-  const queriesParams = searchParams.get('q');
-
-  const filterState = useRecoilValue(FilterState);
-  const resetFilterState = useResetRecoilState(FilterState);
-  const { page, queries } = useRecoilValue(FilterStatsState);
-  const setPageState = useSetRecoilState(PageState);
+  const page = Number(searchParams.get('page')) || 0;
+  const queriesParams = searchParams.get('q') || '';
+  const queries = document.location.search ? queriesParams : OPEN_QUERY;
 
   const { useIssuesData } = useFetchIssue();
-  const { data: issues } = useIssuesData(pageParams, queriesParams);
+  const { data: issues } = useIssuesData(page, queries);
 
-  const { setIssueState, setParsingFilterState } = useFilter();
+  const setFilterState = useSetRecoilState(FilterState);
+  const setPageState = useSetRecoilState(PageState);
 
   const setURLQueriesToFilterState = () => {
-    if (!document.location.search) return;
-    resetFilterState();
-
-    setIssueState(queriesParams);
-
-    const queriesArr = queriesParams?.match(parsingFilterReg);
-    queriesArr?.forEach((query) => {
-      setParsingFilterState(query);
-    });
+    setPageState(page);
+    setFilterState(queries);
   };
 
-  // 쿼리가 변경되면 해당하는 결과로 이동한다.
   useEffect(() => {
-    if (!document.location.search && filterState === initFilterState) return;
-    navigate(`/issues?page=${page}&q=${queries}`);
+    setURLQueriesToFilterState();
   }, [queries]);
-
-  useEffect(() => {
-    setPageState(pageParams);
-
-    // 뒤로가기 시 url의 쿼리와 FilterState를 일치시킨다.
-    if (decodeURIComponent(queries).replaceAll('+', ' ') !== decodeURIComponent(queriesParams!)) {
-      setURLQueriesToFilterState();
-    }
-    // 이전 페이지가 루트인 경우 FilterState를 초기화한다.
-    if (!queriesParams && !pageParams) {
-      resetFilterState();
-    }
-  }, [queriesParams]);
 
   return (
     <>
       <IssuesNavInline />
       <IssueTable issuesData={issues!} />
-      {!!issues!.issues.content.length && (
-        <Paginiation totalPages={issues!.issues.totalPages} currentPage={pageParams} />
-      )}
+      {!!issues!.issues.content.length && <Paginiation totalPages={issues!.issues.totalPages} currentPage={page} />}
     </>
   );
 };
--- src/stores/filter.ts
+++ src/stores/filter.ts
@@ -1,35 +1,9 @@
 import { atom, selector } from 'recoil';
-import { LoginUserInfoState } from '@/stores/loginUserInfo';
-import { parsingFilterReg } from '@/hooks/useFilter';
+import { OPEN_QUERY } from '@/hooks/useFilter';
 
-export type IssueStateType = 'open' | 'closed' | 'all';
-export type IssueAboutMyselfType = '@me';
-export type NoFilterKeysType = 'assignee' | 'label' | 'milestone';
-
-export interface FilterStateTypes {
-  [key: string]: string | string[];
-  is: IssueStateType;
-  mentions: string | IssueAboutMyselfType;
-  author: string | IssueAboutMyselfType;
-  assignee: string | IssueAboutMyselfType;
-  label: string[];
-  milestone: string;
-  no: NoFilterKeysType[];
-}
-
-export const initFilterState: FilterStateTypes = {
-  is: 'open',
-  author: '',
-  assignee: '',
-  mentions: '',
-  label: [],
-  milestone: '',
-  no: [],
-};
-
-export const FilterState = atom<FilterStateTypes>({
+export const FilterState = atom<string>({
   key: 'FilterState',
-  default: initFilterState,
+  default: OPEN_QUERY,
 });
 
 export const PageState = atom<number>({
@@ -37,54 +11,12 @@ export const PageState = atom<number>({
   default: 0,
 });
 
-const engReg = /^[a-zA-Z]*$/g;
-
 export const FilterStatsState = selector({
   key: 'FilterStatsState',
   get: ({ get }) => {
     const filterState = get(FilterState);
-    const userInfo = get(LoginUserInfoState);
-    const page = get(PageState);
+    const isFiltering = filterState !== OPEN_QUERY;
 
-    const isFiltering = JSON.stringify(filterState) !== JSON.stringify(initFilterState);
-
-    const defineFilterString = (type: 'FILTER_BAR' | 'QUERY') =>
-      Object.keys(filterState)
-        .reduce((acc, key) => {
-          const value = filterState[key];
-
-          if (Array.isArray(value)) {
-            return (
-              acc +
-              value.reduce((accQuery: string, curValue: string) => {
-                if (type === 'FILTER_BAR' && curValue.match(engReg)) return `${accQuery} ${key}:${curValue}`;
-                if (type === 'QUERY' && key === 'no') return `${accQuery} ${curValue}:""`;
-
-                return `${accQuery} ${key}:"${curValue}"`;
-              }, '')
-            );
-          }
-
-          if (typeof value === 'string' && value) {
-            if (key === 'is' && value === 'all') return acc;
-            if (type === 'FILTER_BAR' && (value.match(engReg) || value === '@me')) return `${acc} ${key}:${value}`;
-            if (type === 'QUERY' && value === '@me') return `${acc} ${key}:"${userInfo.nickname}"`;
-
-            return `${acc} ${key}:"${value}"`;
-          }
-
-          return acc;
-        }, '')
-        .trim();
-
-    const filterBarString = defineFilterString('FILTER_BAR');
-    const filterQueryString = defineFilterString('QUERY');
-    const queries =
-      filterQueryString
-        .match(parsingFilterReg)
-        ?.map((e) => encodeURIComponent(e))
-        .join('+') || '';
-
-    return { isFiltering, page, filterBarString, queries };
+    return { isFiltering };
   },
 });
