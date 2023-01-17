7283e2c383fff6c7bd0cbcb9dc995e730194f4af
jm <jm.dev.study@gmail.com>
2023-01-17 17:03:24 +0900

    💄 /issues 페이지(루트)에 대한 반응형 화면 추가 - 모바일 화면일 경우 이슈테이블의 체크박스와 열린/닫힌 이슈 버튼을 없앰 (깃허브 참고) - 너비가 380px 이하일 경우 필터 드롭다운의 텍스트가 말줄임표로 변함 - 너비가 500px 이하일 경우 담당자의 이미지가 보이지 않게 처리

--- src/components/Organisms/IssueTable/IssueItem/index.styles.ts
+++ src/components/Organisms/IssueTable/IssueItem/index.styles.ts
@@ -1,13 +1,37 @@
 import { Label } from '@/components/Atoms/Label/index.styles';
 import styled from 'styled-components';
 
-export const StyledIssueItem = styled.div`
-  ${({ theme }) => theme.MIXIN.FLEX({ align: 'center', justify: 'flex-start' })};
-  background: ${({ theme }) => theme.COLORS.OFF_WHITE};
+export const Template = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 60px auto 100px;
 
   .checkbox {
     margin-top: -35px;
   }
+
+  @media ${({ theme }) => theme.DEVICE.MOBILE} {
+    grid-template-columns: auto auto;
+
+    .checkbox {
+      display: none;
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    img {
+      display: none;
+    }
+
+    .summary {
+      margin: 0px;
+    }
+  }
+`;
+
+export const StyledIssueItem = styled.div`
+  ${({ theme }) => theme.MIXIN.FLEX({ align: 'center', justify: 'flex-start' })};
+  background: ${({ theme }) => theme.COLORS.OFF_WHITE};
 `;
 
 export const StyledIssue = styled.div`
@@ -32,6 +56,25 @@ export const IssueTitle = styled.div`
 
   ${Label}+${Label} {
     margin-left: 8px;
+  }
+
+  @media ${({ theme }) => theme.DEVICE.MOBILE} {
+    width: 100%;
+    grid-template-columns: repeat(2, auto);
+
+    div {
+      grid-column: 1 / 4;
+      overflow: scroll;
+
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+
+    .title {
+      ${({ theme }) => theme.FONTSTYLES.LINK_MEDIUM};
+      margin-bottom: 8px;
+    }
   }
 `;
 
@@ -65,21 +108,25 @@ export const IssueContent = styled.div`
       fill: ${({ theme }) => theme.COLORS.LABEL};
     }
   }
+
+  @media ${({ theme }) => theme.DEVICE.MOBILE} {
+    align-items: flex-start;
+    .summary {
+      margin: 0px;
+    }
+    .milestone {
+      display: none;
+    }
+
+    span:first-child {
+      margin-right: 8px;
+    }
+  }
 `;
 
 export const Assignee = styled.div`
   margin-left: auto;
   img + img {
     margin-left: -10px;
-  }
-`;
-
-export const Template = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 60px auto 100px;
-
-  .checkbox {
-    margin-top: -35px;
   }
 `;
