78fcf6792a85ff7b519d8d733e4e65e4d1aa96d3
연 <mogoo22@naver.com>
2023-01-19 21:36:34 +0900

    📱 페이지 padding을 반응형 분기점에 따라 적용함

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,8 +12,20 @@ const StyledLayer = styled.div`
   height: 100vh;
 
   & > div {
-    width: 1280px;
+    max-width: 1280px;
     margin: 0 auto;
+
+    @media all and (min-width: 1024px) {
+      padding: 27px 32px 27px 32px;
+    }
+
+    @media all and (min-width: 768px) and (max-width: 1023px) {
+      padding: 18px 24px 18px 24px;
+    }
+
+    @media all and (max-width: 767px) {
+      padding: 8px 16px 8px 16px;
+    }
   }
 `;
 
