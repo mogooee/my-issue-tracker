09bd55fa39ffbb90799c3bc6752f2f82fe72f4c6
연 <mogoo22@naver.com>
2023-01-26 17:57:36 +0900

    📱 로그인, 일반 회원가입 반응형 구현
    Co-authored-by: 도비 <JiminKim-dev@users.noreply.github.com>

--- src/pages/Public/Login.tsx
+++ src/pages/Public/Login.tsx
@@ -4,6 +4,7 @@ import Logo from '@/components/Atoms/Logo';
 
 const StyledLogin = styled.div`
   ${({ theme }) => theme.MIXIN.FLEX({ direction: 'column', align: 'center', justify: 'center' })};
+  width: 100%;
   height: 100vh;
 
   & > a:first-child {
