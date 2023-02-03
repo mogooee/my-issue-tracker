03017d2f7ac91e14362152b46854d040e38b45ef
jm <jm.dev.study@gmail.com>
2023-02-03 14:57:05 +0900

    📖 Login Page에 대한 스토리 추가

---
+++ src/pages/Public/Login/Login.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Routes, Route } from 'react-router-dom';
+import { authHandlers } from '@/mocks/handlers/auth';
+import Login from '@/pages/Public/Login';
+import CommonSignUp from '@/pages/Public//SignUp-Common';
+
+export default {
+  title: 'pages (Public)/Login',
+  component: Login,
+  decorators: [
+    (Story) => (
+      <Routes>
+        <Route path="/" element={<Story />} />
+        <Route path="/signup" element={<CommonSignUp />} />
+      </Routes>
+    ),
+  ],
+} as ComponentMeta<typeof Login>;
+
+const Template: ComponentStory<typeof Login> = () => <Login />;
+
+export const Initial = Template.bind({});
+Initial.parameters = {
+  msw: {
+    handlers: [...authHandlers],
+  },
+};
