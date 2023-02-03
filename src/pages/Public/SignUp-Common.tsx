2d9f06b416b5e9bcb53895009c5e4c82c232140e
jm <jm.dev.study@gmail.com>
2023-02-03 14:33:38 +0900

    💄 CommonSignUp 컴포넌트 스타일 수정 - 중앙 배치로 수정 - 모바일 화면에서 margin-top 추가

--- src/pages/Public/SignUp-Common.tsx
+++ src/pages/Public/SignUp-Common.tsx
@@ -11,9 +11,16 @@ import CompleteSignUp from '@/components/Modal/CompleteSignUp';
 
 const StyledDiv = styled.div`
   ${({ theme }) => theme.MIXIN.FLEX({ direction: 'column', align: 'center', justify: 'center' })};
+  width: 100%;
+  height: 100vh;
 
   & > a:first-child {
     margin-bottom: 64px;
+  }
+
+  @media ${({ theme }) => theme.DEVICE.MOBILE} {
+    margin-top: 60px;
+    height: 100%;
   }
 `;
 
