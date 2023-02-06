4370bc4efc31eedce31a9650b6daf639b16cdf33
jm <jm.dev.study@gmail.com>
2023-02-06 14:15:33 +0900

    📖 Modal과 관련된 스토리 추가

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,43 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Modal from '@/components/Modal';
+import CancelNewIssueModal from '@/components/Modal/CancelNewIssue';
+import CompleteSignUpModal from '@/components/Modal/CompleteSignUp';
+import DeleteCheckModal from '@/components/Modal/DeleteCheck';
+import ServiceLoadingModal from '@/components/Modal/ServiceLoading';
+
+export default {
+  title: 'Modal',
+  component: Modal,
+} as ComponentMeta<typeof Modal>;
+
+const CancelNewIssueTemplate: ComponentStory<typeof Modal> = () => (
+  <Modal>
+    <CancelNewIssueModal />
+  </Modal>
+);
+
+export const CancelNewIssue = CancelNewIssueTemplate.bind({});
+
+const CompleteSignUpTemplate: ComponentStory<typeof Modal> = () => (
+  <Modal>
+    <CompleteSignUpModal id="newUser" />
+  </Modal>
+);
+
+export const CompleteSignUp = CompleteSignUpTemplate.bind({});
+
+const DeleteCheckTemplate: ComponentStory<typeof Modal> = () => (
+  <Modal>
+    <DeleteCheckModal handleDeleteButtonClick={() => {}} />
+  </Modal>
+);
+
+export const DeleteCheck = DeleteCheckTemplate.bind({});
+
+const ServiceLoadingTemplete: ComponentStory<typeof Modal> = () => (
+  <Modal>
+    <ServiceLoadingModal />
+  </Modal>
+);
+
+export const ServiceLoading = ServiceLoadingTemplete.bind({});
