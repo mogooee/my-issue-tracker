3f3bdffae01e555933462c053307cf3b1addbd37
도비 <jm.dev.study@gmail.com>
2023-04-30 01:28:15 +0900

    🔧 test 환경에서 이미지 import를 하기 위한 config 설정 추가 및 파일 추가 - 공식 문서(웹팩과 사용하기 챕터) 참고

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
 module.exports = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
+  modulePaths: ['/shared/vendor/modules'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleDirectories: ['node_modules', 'bower_components', 'shared'],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)?$': 'babel-jest',
     '^.+\\.svg$': 'jest-transformer-svg',
+    '\\.(png|jpg|jpeg|gif|eot|otf|webp|ttf|woff|woff2)$': '<rootDir>/fileTransformer.js',
   },
   moduleNameMapper: {
     '^@/(.*svg)(\\?inline)$': '<rootDir>/src/$1',
     '^@/(.*svg)(\\?component)$': '<rootDir>/src/$1',
     '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(png|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
   },
   setupFilesAfterEnv: ['./setupTests.ts'],
 };
