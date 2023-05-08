f1042fd789e195b59331b0daa08ea91700420d16
도비 <jm.dev.study@gmail.com>
2023-05-09 00:44:51 +0900

    🚚 Token과 관련된 상수를 mock 폴더 내부의 constants 파일로 이동

---
+++ src/mocks/constants.ts
@@ -0,0 +1,9 @@
+export const ACCESS_TOKEN = {
+  KEY: 'access_token',
+  VALUE: 'access123',
+};
+
+export const REFRESH_TOKEN = {
+  KEY: 'refresh_token',
+  VALUE: 'refresh123',
+};
--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -5,6 +5,7 @@ import { USER_LIST as OAUTH_USER_LIST } from '@/components/Molecules/Dropdown/mo
 import { UserTypes } from '@/api/issue/types';
 import AppLogo from '@/assets/logo/issueTracker.png';
 import { ERROR_CODE } from '@/api/constants';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '@/mocks/constants';
 
 interface GeneralUserInfoTypes {
   loginId?: string;
@@ -24,16 +25,6 @@ export const USER_TABLE: UserTableTypes[] = [
     profileImage: AppLogo,
   },
 ];
-
-const ACCESS_TOKEN = {
-  KEY: 'access_token',
-  VALUE: 'access123',
-};
-
-const REFRESH_TOKEN = {
-  KEY: 'refresh_token',
-  VALUE: 'refresh123',
-};
 
 const getCookie = (key: string, value: string) => {
   const matchKey = document.cookie.match(`(^|;) ?${key}=([^;]*)(;|$)`);
